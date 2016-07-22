7ac6e62bc9296db36480df927e84deaf8fd640a7
Gileno Filho <contato@gilenofilho.com.br>
2016-07-22 16:28:26 -0300

    adicionando testes para as views

---
+++ djangoecommerce/core/tests/test_views.py
@@ -0,0 +1,28 @@
+# coding=utf-8
+
+from django.test import TestCase, Client
+from django.core.urlresolvers import reverse
+
+
+class ViewsTestCase(TestCase):
+
+    def setUp(self):
+        self.url_index = reverse('index')
+        self.url_products = reverse('products')
+        self.url_contact = reverse('contact')
+        self.client = Client()
+
+    def test_index_view(self):
+        response = self.client.get(self.url_index)
+        self.assertEquals(response.status_code, 200)
+        self.assertTemplateUsed(response, 'index.html')
+
+    def test_index_products(self):
+        response = self.client.get(self.url_products)
+        self.assertEquals(response.status_code, 200)
+        self.assertTemplateUsed(response, 'products.html')
+
+    def test_index_contact(self):
+        response = self.client.get(self.url_contact)
+        self.assertEquals(response.status_code, 200)
+        self.assertTemplateUsed(response, 'contact.html')

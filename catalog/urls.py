78b51d61251ea77617c99c57f98ba363680672f1
Gileno Filho <contato@gilenofilho.com.br>
2016-07-25 15:44:21 -0300

    adicionando exibição do produto e exibição dos produtos de uma categoria

--- catalog/urls.py
+++ catalog/urls.py
@@ -6,4 +6,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^categorias/([\w_-]+)/$', views.category, name='category'),
+    url(r'^produtos/([\w_-]+)/$', views.product, name='product'),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,6 +1,6 @@
 # coding=utf-8
 
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from .models import Product, Category
 
@@ -10,3 +10,20 @@ def index(request):
         'products': Product.objects.all(),
     }
     return render(request, 'catalog/index.html', context)
+
+
+def category(request, slug):
+    category = get_object_or_404(Category, slug=slug)
+    context = {
+        'category': category,
+        'products': Product.objects.filter(category=category)
+    }
+    return render(request, 'catalog/category.html', context)
+
+
+def product(request, slug):
+    product = get_object_or_404(Product, slug=slug)
+    context = {
+        'product': product
+    }
+    return render(request, 'catalog/product.html', context)
